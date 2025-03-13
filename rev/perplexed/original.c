607cd7cee0eef5d0a3f221e934e91e84d2c092e0
asatpathy314 <asatpathy314@gmail.com>
2025-03-13 09:37:15 -0700

    reversing

--- rev/perplexed/original.c
+++ rev/perplexed/original.c
@@ -1,39 +1,73 @@
-long long check(char* input)
+#include <stddef.h>
+#define uint unsigned int
+
+uint check(char *param_1)
 {
-    if (strlen(input) != 0x1b)
-        return 1;
-    
-    long long enc_flag;
-    __builtin_memcpy(&enc_flag, "\xe1\xa7\x1e\xf8\x75\x23\x7b\x61\xb9\x9d\xfc\x5a\x5b\xdf\x69\xd2\xfe\x1b\xed\xf4\xed\x67\xf4", 0x17);
-    long var_1c_1 = 0;
-    long var_20_1 = 0;
-    long var_2c_1 = 0;
-    
-    for (long i = 0; i <= 22; i += 1)
-    {
-        for (long j = 0; j <= 7; j += 1)
-        {
-            if (!var_20_1)
-                var_20_1 += 1;
-            
-            long rax_17;
-            rax_17 = (input[var_1c_1] & 1 << (7 - var_20_1)) > 0;
-            
-            if (rax_17 != (*(&enc_flag + i) & 1 << (7 - j)) > 0)
-                return 1;
-            
-            var_20_1 += 1;
-            
-            if (var_20_1 == 8)
-            {
-                var_20_1 = 0;
-                var_1c_1 += 1;
-            }
-            
-            if (var_1c_1 == strlen(input))
-                return 0;
+  size_t sVar1;
+  uint uVar2;
+  size_t sVar3;
+  char local_58 [36];
+  uint local_34;
+  uint local_30;
+  int local_28;
+  uint local_24;
+  int local_20;
+  int local_1c;
+  
+  sVar1 = strlen(param_1);
+  if (sVar1 == 0x1b) {
+    local_58[0] = -0x1f;
+    local_58[1] = -0x59;
+    local_58[2] = '\x1e';
+    local_58[3] = -8;
+    local_58[4] = 'u';
+    local_58[5] = '#';
+    local_58[6] = '{';
+    local_58[7] = 'a';
+    local_58[8] = -0x47;
+    local_58[9] = -99;
+    local_58[10] = -4;
+    local_58[0xb] = 'Z';
+    local_58[0xc] = '[';
+    local_58[0xd] = -0x21;
+    local_58[0xe] = 'i';
+    local_58[0xf] = 0xd2;
+    local_58[0x10] = -2;
+    local_58[0x11] = '\x1b';
+    local_58[0x12] = -0x13;
+    local_58[0x13] = -0xc;
+    local_58[0x14] = -0x13;
+    local_58[0x15] = 'g';
+    local_58[0x16] = -0xc;
+    local_1c = 0;
+    local_20 = 0;
+    for (local_24 = 0; local_24 < 0x17; local_24 = local_24 + 1) {
+      for (local_28 = 0; local_28 < 8; local_28 = local_28 + 1) {
+        if (local_20 == 0) {
+          local_20 = 1;
         }
+        local_30 = 1 << (7U - (char)local_28 & 0x1f);
+        local_34 = 1 << (7U - (char)local_20 & 0x1f);
+        if (0 < (int)((int)param_1[local_1c] & local_34) !=
+            0 < (int)((int)local_58[(int)local_24] & local_30)) {
+          return 1;
+        }
+        local_20 = local_20 + 1;
+        if (local_20 == 8) {
+          local_20 = 0;
+          local_1c = local_1c + 1;
+        }
+        sVar3 = (size_t)local_1c;
+        sVar1 = strlen(param_1);
+        if (sVar3 == sVar1) {
+          return 0;
+        }
+      }
     }
-    
-    return 0;
+    uVar2 = 0;
+  }
+  else {
+    uVar2 = 1;
+  }
+  return uVar2;
 }
--- rev/perplexed/reverse.py
+++ rev/perplexed/reverse.py
@@ -1,8 +1,43 @@
-flag = b"\xe1\xa7\x1e\xf8\x75\x23\x7b\x61\xb9\x9d\xfc\x5a\x5b\xdf\x69\xd2\xfe\x1b\xed\xf4\xed\x67\xf4"
+def reverse_engineer_flag():
+    # Encoded flag from the C code
+    enc_flag = [
+        0xE1, 0xA7, 0x1E, 0xF8, 0x75, 0x23, 0x7B, 0x61, 
+        0xB9, 0x9D, 0xFC, 0x5A, 0x5B, 0xDF, 0x69, 0xD2, 
+        0xFE, 0x1B, 0xED, 0xF4, 0xED, 0x67, 0xF4
+    ]
+    
+    # Convert to binary representation
+    enc_bits = []
+    for byte in enc_flag:
+        # Convert to 8-bit binary representation
+        bits = [(byte >> i) & 1 for i in range(7, -1, -1)]
+        enc_bits.extend(bits)
+    
+    # Reconstruct the flag
+    flag_bits = []
+    local_20 = 0
+    
+    for enc_bit in enc_bits:
+        if local_20 == 0:
+            local_20 = 1
+        
+        flag_bits.append(enc_bit)
+        
+        local_20 += 1
+        if local_20 == 8:
+            local_20 = 0
+    
+    # Convert bits to bytes
+    flag_bytes = []
+    for i in range(0, len(flag_bits), 8):
+        byte = 0
+        for j in range(8):
+            if i + j < len(flag_bits):
+                byte = (byte << 1) | flag_bits[i + j]
+        flag_bytes.append(byte)
+    
+    # Convert bytes to ASCII
+    flag = ''.join(chr(b) for b in flag_bytes)
+    return flag
 
-binary = []
-for byte in flag:
-    binary.append(bin(int(byte))[2:][::-1])
-
-binary = [chr(int(char, 2)) for char in binary]
-print(''.join(binary))
+print(reverse_engineer_flag())
